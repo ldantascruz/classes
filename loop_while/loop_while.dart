8a935930da0e0bd7b41a1034a328d95e4375271a
Lucas <ldantascruz@gmail.com>
2022-06-23 14:04:24 -0300

    commit switch and while

---
+++ loop_while/loop_while.dart
@@ -0,0 +1,28 @@
+void main() {
+  /*
+  int i = 0;
+
+  while (i <= 100) {
+    print(i);
+    i++;
+  }
+  print("Terminou!");
+
+  int j = 100;
+
+  while (j >= 0) {
+    print(j);
+    j--;
+  }
+  print("Terminou!");
+*/
+  int a = 15;
+  int b = 900;
+
+  while (a >= -10 || b >= 800) {
+    print(a);
+    print(b);
+    a--;
+    b = b-10;
+  }
+}
---
+++ switch/switch.dart
@@ -0,0 +1,17 @@
+void main() {
+  String linguagem = "C#";
+
+  switch (linguagem) {
+    case 'Dart':
+      print("Linguagem muito legal!!!");
+      break;
+    case 'C':
+      print("Linguagem muito complicada");
+      break;
+    case 'Swift':
+      print("Linguagem muito complicada também");
+      break;
+    default:
+      print("Linguagem desconhecida");
+  }
+}

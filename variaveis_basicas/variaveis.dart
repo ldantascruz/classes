602dbce85b2f9ace8a5bf841e6578a906a4bec7a
Lucas <ldantascruz@gmail.com>
2022-06-23 06:26:28 -0300

    commit on udemy dart and flutter course

---
+++ variaveis_basicas/variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  int idade = 22; // variável do tipo inteiro
+  String nome = "Lucas"; // variável do tipo string
+  double distancia = 24.89; // variável do tipo double
+
+  print(idade);
+  print(nome);
+  print(distancia);
+
+  idade = 23; // alteração da variável idade de 22 para 23
+  nome = "Cruz"; // alteração da variável nome de "Lucas" para "Cruz"
+
+  print("O ${nome} que tem ${idade} anos, correu ${distancia}km");
+}
